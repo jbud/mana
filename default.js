75f2abc635c85743c82bf51e7e0f274854281c72
Joe Jackson <jbudorg@gmail.com>
2018-10-25 18:53:25 -0700

    efficiency rewrite of base-code
    * deprecated all old functions
    * renamed some functions to better reflect their purpose
    * use global elements array for simplicity through multiple functions
    * fix for use of restricted code-words as variables in javascript.
    "element(s)"
    * efficiency fixes by using memory instead of executing same code
    multiple times (as functions like calc2() and calculate() are called
    multiple times per second this can improve performance massively)

--- default.js
+++ default.js
@@ -1,8 +1,8 @@
 /*
-12/2/2011
+#12/2/2011
 *Initial Release as Windows Appx App
-
-10/25/2018
+*
+#10/25/2018
 * Updated for web and added to budzique.com/mtg.
 * Added web-app capibility for iOS (tap share and tap add to home screen in safari)
 * Revised input method to not be tedious (previously you had to tap buttons to add symbols and could not go backwards so only reset to 0 would help) by adding forms instead
@@ -10,131 +10,81 @@
 * Land slider will now update calculation on slide.
 * When even number of mana symbols produce 1 additional land than desired (such as 8 forest and 8 plains when you only wanted 15 lands) one land will now be removed at random.
 * Land slider now has numeric output (This was previously removed to hide the additional land bug fixed above)
-
-
-
-
-
 */
-var glob_id;
-function setVal(element, val) { /* Please deprecate */
-        var id = document.getElementById(element);
-        id.innerHTML = val;
+var gId;
+var gElements = ["W", "U", "B", "R", "G"];
+function getInner(iElement) { 
+    gId = document.getElementById(iElement);
+    return eval(gId.innerHTML);
 }
-function setVal2(element, val) {
-        var id = document.getElementById(element);
-        id.value = val;
+function setInner(iElement, val) { 
+    gId = document.getElementById(iElement);
+    gId.innerHTML = val;
 }
-function getVal(element) { /* Please deprecate */
-        glob_id = document.getElementById(element);
-        
-    return eval(glob_id.innerHTML);
+function getVal(iElement) {
+    gId = document.getElementById(iElement);
+    return eval(gId.value);
 }
-function getVal2(element) {
-    glob_id = document.getElementById(element);
-    return eval(glob_id.value);
-    
+function setVal(iElement, val) {
+    gId = document.getElementById(iElement);
+    gId.value = val;
 }
-// replacement for totalManaSymbols()
-function totalManaSymbols2(){
-    var elements = ["W", "U", "B", "R", "G"];
+function totalManaSymbols2() {
     var r = 0;
     for(var i=0;i<5;i++){
-        r += getVal2(getElementByColor(elements[i], false));
+        r += getVal(getElementByColor2(gElements[i], false));
     }
     return r;
 }
-// replacement for totalManaSymbolsL()
-function totalManaSources2(){
-    var elements = ["W", "U", "B", "R", "G"];
+function totalManaSources2() {
     var r = 0;
     for(var i=0;i<5;i++){
-        r += getVal(getElementByColor(elements[i], true));
+        r += getInner(getElementByColor2(gElements[i], true));
     }
     return r;
 }
-function totalManaSymbols() { /* Please deprecate */
-    return getVal2(getElementByColor("W", false)) + getVal2(getElementByColor("U", false)) + getVal2(getElementByColor("B", false)) + getVal2(getElementByColor("R", false)) + getVal2(getElementByColor("G", false));
-
-}
-function totalManaSymbolsL() { /* Please deprecate */
-    return getVal(getElementByColor("W", true)) + getVal(getElementByColor("U", true)) + getVal(getElementByColor("B", true)) + getVal(getElementByColor("R", true)) + getVal(getElementByColor("G", true));
-
-}
+/**
+ * void fx()
+ * 
+ * An even number of symbols with odd lands caused an overflow on lands, 
+ * remove one from a color at random. (ignoring those with 0 symbols)
+ *
+ */
 function fx(){ 
-    
-    /* an even number caused an overflow on lands, 
-    remove one from a color at random. (ignoring those with 0 symbols) */
-    var elements = ["W", "U", "B", "R", "G"];
     var t = 0;
     var x = 0;
     var l = [];
     for(var i = 0; i<5; i++) {
-        t = parseInt(getVal2(getElementByColor(elements[i], false)));
+        t = parseInt(getVal(getElementByColor2(gElements[i], false)));
         if (t != 0) {
-            l[x] = elements[i];
+            l[x] = gElements[i];
             x++;
         }
     }
     var z = Math.floor(Math.random() * l.length);
     var el = l[z];
-    t = getVal(getElementByColor(l[z], true));
-    setVal(getElementByColor(l[z],true), t - 1);
+    t = getInner(getElementByColor2(l[z], true));
+    setInner(getElementByColor2(l[z],true), t - 1);
 }
 function calc2(){
-    var elements = ["W", "U", "B", "R", "G"];
     var s = 0;
     var c;
     for(var i=0;i<5;i++){
-        c = elements[i];
-        s = getVal2(getElementByColor(c, false));
+        c = gElements[i];
+        s = getVal(getElementByColor2(c, false));
         calculate(c,s);
     }
-    if (totalManaSymbolsL() > getVal("lnd")){
+    if (totalManaSources2() > getInner("lnd")){
         fx();
     }
 }
-function calc() { /* Please deprecate */
-    var symbols = getVal2(getElementByColor("W", false));
-    var color = "W";
-    calculate(color, symbols);
-    symbols = getVal2(getElementByColor("U", false));
-    color = "U";
-    calculate(color, symbols);
-    symbols = getVal2(getElementByColor("B", false));
-    color = "B";
-    calculate(color, symbols);
-    symbols = getVal2(getElementByColor("R", false));
-    color = "R";
-    calculate(color, symbols);
-    symbols = getVal2(getElementByColor("G", false));
-    color = "G";
-    calculate(color, symbols);
-    if (totalManaSymbolsL() > getVal("lnd")){
-        fx();
-    }
-}
-function rst2(){
-    var elements = ["W", "U", "B", "R", "G"];
+function rst(){
     var c;
     for(var i=0;i<5;i++){
-        c = elements[i];
-        setVal2(getElementByColor(c,false),0);
-        setVal(getElementByColor(c,true),0);
+        c = gElements[i];
+        setVal(getElementByColor2(c,false),0);
+        setInner(getElementByColor2(c,true),0);
     }
-}
-function rst(){ /* Please deprecate */
-    setVal2(getElementByColor("W", false), 0);
-    setVal2(getElementByColor("U", false), 0);
-    setVal2(getElementByColor("B", false), 0);
-    setVal2(getElementByColor("R", false), 0);
-    setVal2(getElementByColor("G", false), 0);
-    setVal(getElementByColor("W", true), 0);
-    setVal(getElementByColor("U", true), 0);
-    setVal(getElementByColor("B", true), 0);
-    setVal(getElementByColor("R", true), 0);
-    setVal(getElementByColor("G", true), 0);
-
 }
 /**
  * void calculate(str color, int symbols)
@@ -146,17 +96,16 @@ function rst(){ /* Please deprecate */
  *
  */
 function calculate(color, symbols) {
-    var id = document.getElementById("land");
-    lands = parseInt(id.value);
-    var sources = Math.round(getVal2(getElementByColor(color, false)) * lands / totalManaSymbols());
-    if (totalManaSymbols() == 0){
+    var lands = parseInt(getInner("lnd"));
+	var totSym = totalManaSymbols2();
+    var sources = Math.round(getVal(getElementByColor2(color, false)) * lands / totSym);
+    if (totSym == 0){
         sources = 0;
     }
     if (isNaN(sources)){
         sources = 0;
     }
-    var element = getElementByColor(color, true);
-    setVal(element, sources);
+    setInner(getElementByColor2(color, true), sources);
 }
 /**
  * str getElementByColor2(str color, bool isSource)
@@ -177,54 +126,11 @@ function getElementByColor2(color, isSource){
     };
     return (isSource) ? sources[color] : symbols[color];
 }
-/**
- * str getElementByColor(str color, bool isSource)
- * str color - either "W", "U", "B", "R", "G"
- * bool isSource - whether we are returning the id for source or symbol
- * return: str - id of the element
- */
-function getElementByColor(color,isSource) { /* Please deprecate */
-    switch(color)
-    {
-        case "W":
-            if (isSource)
-                return "whiteSource";
-            else
-                return "whiteSym";
-            break;
-        case "U":
-            if (isSource)
-                return "blueSource";
-            else
-                return "blueSym";
-            break;
-        case "B":
-            if (isSource)
-                return "blackSource";
-            else
-                return "blackSym";
-            break;
-        case "R":
-            if (isSource)
-                return "redSource";
-            else
-                return "redSym";
-            break;
-        case "G":
-            if (isSource)
-                return "greenSource";
-            else
-                return "greenSym";
-            break;
-        default:
-            return false;
-    }
 
-}
 function onChng(val){
-    setVal("lnd", val);
-    calc();
+    setInner("lnd", val);
+    calc2();
 }
-function clr(element){
-    setVal2(element, "");
+function clr(iElement){
+    setVal(iElement, "");
 }
